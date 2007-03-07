97dd59e17d7a9ba1be7d8f65575aaf0c02dc4d18
jjconti <jjconti@c678a081-2629-0410-8648-d7026fe227d8>
2007-03-07 16:39:29 +0000

    Add the BUGS file and make clear.sh more usefull.

--- clear.sh
+++ clear.sh
@@ -1,2 +1,11 @@
-rm *~
-rm *.bak
+# Just a developer short cut to keep working copy free of temporary files.
+rm *~ 2> /dev/null
+rm *.bak 2> /dev/null
+rm \#*\# 2> /dev/null
+
+if [ -n "$1" ];
+	then
+		if [ "$1" = "all" ];
+			then rm *.pyc 2> /dev/null
+		fi
+fi
